42d5c14a8ee9514be39d9eed3d0a380cebff1613
zhangsy <1270985196@qq.com>
2025-05-06 17:57:24 +0800

    类型

--- base.js
+++ base.js
@@ -1,1 +1,13 @@
-console.log('111');
+// 类型注释
+var age = 19;
+var myName = 'zsy';
+var obj = {
+    number: 12,
+    name: 'zsy',
+};
+obj.name = 'ss';
+console.log(age, myName, obj);
+function add(num1, num2) {
+    return num1 + num2;
+}
+console.log(add('1' * 1, 2));
--- base.ts
+++ base.ts
@@ -1,2 +1,19 @@
-console.log('111')
-console.log('111')
+// 类型注释
+let age: number =19;
+let myName: string ='zsy';
+
+const obj = {
+    number: 12,
+    name: 'zsy',
+}
+const arr  = [{}, 2];
+obj.name = 'ss';
+console.log(age, myName, obj);
+function add(num1:number, num2: number) :number {
+    return num1 + num2;
+}
+// 类型别名
+type MyUserName = string | number;
+let a: MyUserName = 'sss'
+a = 10;
+console.log(add( 1, 2))
